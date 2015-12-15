867e54b5888b7f6db0df676929f5a8a04f4753d8
codenever <cars2991@gmail.com>
2015-12-15 01:21:14 -0500

    fivenumberproduct finished

---
+++ JavaOutput/src/FiveNumberProduct.java
@@ -0,0 +1,17 @@
+
+public class FiveNumberProduct {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	      int number1 = 11;  // Declare 5 int variables to hold 5 integers
+	      int number2 = 22;
+	      int number3 = 33;
+	      int number4 = 44;
+	      int number5 = 55;
+	      int product;           // Declare an int variable called sum to hold the sum
+	      product = number1 * number2 * number3 * number4 * number5;
+	      System.out.print("The sum is ");   // Print a descriptive string
+	      System.out.println(product);           // Print the value stored in product
+	}
+
+}
