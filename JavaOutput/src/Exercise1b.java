657aa59f2eff93dc62e790cba57b387582f4542e
codenever <cars2991@gmail.com>
2015-12-15 01:13:07 -0500

    1b and 1c finished

--- JavaOutput/src/Exercise1b.java
+++ JavaOutput/src/Exercise1b.java
@@ -4,16 +4,37 @@ public class Exercise1b {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		for(int x = 0; x < 5; x++){
-			if (x<1 || x>3){
+			if (x==0){
 				//for(int y =0 ; y < 5; y++){
 				System.out.print("* * * * *");
 					
 				//}
 				System.out.println();
 			}
-			else {
+			if (x==1){
 				//for(int y =0 ; y < 5; y++){
-					System.out.print("*       *");
+				System.out.print(" *     * ");
+					
+				//}
+				System.out.println();
+			}
+			if (x==2){
+				//for(int y =0 ; y < 5; y++){
+				System.out.print("  *   *  ");
+					
+				//}
+				System.out.println();
+			}
+			if (x==3){
+				//for(int y =0 ; y < 5; y++){
+				System.out.print("   * *   ");
+					
+				//}
+				System.out.println();
+			}
+			if (x==3){
+				//for(int y =0 ; y < 5; y++){
+				System.out.print("    *     ");
 					
 				//}
 				System.out.println();
---
+++ JavaOutput/src/Exercise1c.java
@@ -0,0 +1,9 @@
+
+public class Exercise1c {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
