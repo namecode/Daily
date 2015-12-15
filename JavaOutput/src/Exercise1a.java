03eb88499f3878e900781454dbf7ee97a2ff99ce
codenever <cars2991@gmail.com>
2015-12-15 00:38:27 -0500

    Exercise1a finished

---
+++ JavaOutput/src/Exercise1a.java
@@ -0,0 +1,24 @@
+
+public class Exercise1a {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int x = 0; x < 5; x++){
+			if (x%2==0){
+				for(int y =0 ; y < 5; y++){
+					System.out.print("* ");
+					
+				}
+				System.out.println();
+			}
+			else {
+				for(int y =0 ; y < 5; y++){
+					System.out.print(" *");
+					
+				}
+				System.out.println();
+			}
+		}
+	}
+
+}
