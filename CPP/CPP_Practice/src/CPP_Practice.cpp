ed39cc6126ec76973014decf7f54da40eeb1eb86
codenever <cars2991@gmail.com>
2015-12-17 02:33:19 -0500

    helloworld

---
+++ CPP/CPP_Practice/src/CPP_Practice.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : CPP_Practice.cpp
+// Author      : namecode
+// Version     :
+// Copyright   : Use all you want
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
